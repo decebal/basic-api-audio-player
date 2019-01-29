ceec9d01a7a1fbc19f391c74eac193da054dd832
Decebal Dobrica (Deci) <decebal1988@gmail.com>
2019-01-29 00:46:35 +0000

    fix repository methods signature

--- src/infrastructure/interfaces/IPlayerRepository.ts
+++ src/infrastructure/interfaces/IPlayerRepository.ts
@@ -1,6 +1,6 @@
 export interface IPlayerRepository<Player> {
   create(item: Player): Promise<boolean>;
-  findOne(userId: string): Promise<Player>;
+  findOne(id: string): Promise<Player>;
   find(item: Player): Promise<Player[]>;
   update(userId: string, item: Player): Promise<boolean>;
 }
--- src/repositories/mongodb/PlayerRepository.ts
+++ src/repositories/mongodb/PlayerRepository.ts
@@ -1,16 +1,15 @@
 import {injectable} from "inversify";
-import {IPlayerRepository} from "../../infrastructure/interfaces/IPlayerRepository";
-import {Player} from "../../model/entities/Player";
-import {User} from "../../model/entities/User";
+import {IPlayerRepository} from "../../infrastructure/interfaces";
+import {Player} from "../../model/entities";
 import {BaseRepository} from "../base/BaseRepository";
 
 @injectable()
 export class PlayerRepository extends BaseRepository<Player> implements IPlayerRepository<Player> {
-  findOne(user: User): Promise<Player> {
+  public async findOne(userId: string): Promise<Player> {
     return undefined;
   }
 
-  update(user: User, item: Player): Promise<boolean> {
+  public async update(userId: string, item: Player): Promise<boolean> {
     return undefined;
   }
 
